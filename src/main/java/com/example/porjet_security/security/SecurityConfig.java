24fa8456c7c3855ff2d4bebad0464cabf51761c7
N89e <nunodafonseca@live.fr>
2025-08-28 10:04:00 +0200

    Ajout du style aux pages html

--- src/main/java/com/example/porjet_security/security/SecurityConfig.java
+++ src/main/java/com/example/porjet_security/security/SecurityConfig.java
@@ -1,20 +1,16 @@
 package com.example.porjet_security.security;
 
-import com.example.porjet_security.services.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 public class SecurityConfig {
-
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -34,7 +30,6 @@ public class SecurityConfig {
                 );
         return http.build();
     }
-
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
